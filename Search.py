7eebcd889cb64f4e19bd5dd787bc195f14a96a17
sujalw <sujal.wattamwar@gmail.com>
2012-12-11 20:30:05 -0500

    added search functionality

--- Search.py
+++ Search.py
@@ -6,7 +6,7 @@ import jinja2
 import os
 import random
 
-#from data import *
+from Data import *
 
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -20,9 +20,59 @@ class Search(webapp2.RequestHandler):
 		self.response.out.write('in get of search')
 
 	def post(self):
-		self.response.out.write('in post of search')
 		user_name = users.get_current_user().nickname()
 		url = users.create_logout_url('/')
 		url_linktext = 'Logout'
 						
 		task_name = self.request.get('task_name')
+		
+		if task_name == "search_items":
+			keywords = self.request.get('keywords')
+			
+			authors = set([])
+						
+			# get all users of the system
+			categories = Category.all()
+			for category in categories:
+				authors.add(category.author)
+			
+			# loop through all authors
+			for author in authors:
+				# search for all categories by current user
+				categories = db.GqlQuery(	"SELECT * "
+																	"FROM Category "
+																	"WHERE ANCESTOR IS :1 ",
+																	category_key(author))
+																	
+				# dict to store search results
+				searchResults = {}
+								
+				for category in categories:
+					# search for all items in each category
+					items = db.GqlQuery(	"SELECT * "
+																"FROM Item "
+																"WHERE ANCESTOR IS :1 ",
+																item_key(user_name, category.name))
+														
+					for item in items:
+						if keywords in item.name:
+							resultInfo = {}
+							resultInfo['category'] = category.name
+							resultInfo['author'] = author
+							
+							searchResults[item.name] = resultInfo
+						
+			
+			#
+			template_values = {
+				'user_name': user_name,
+				'keywords': keywords,
+				'url': url,
+				'url_linktext': url_linktext,
+				'searchResults': searchResults,
+				'back_url': '/',
+				'home_url': '/',
+			}
+		
+			template = jinja_environment.get_template('search.html')
+			self.response.out.write(template.render(template_values))
